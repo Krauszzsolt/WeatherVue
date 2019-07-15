d91d7d5cb2000973a4b92d1b10f51a234031dabf
krausz.zsolt <krausz.zsolt@DEV-NB-0119>
2019-07-15 14:54:59 +0200

    input field

--- src/core/components/login/LoginFields.component.vue
+++ src/core/components/login/LoginFields.component.vue
@@ -10,20 +10,18 @@
                   <v-toolbar-title>Login</v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-tooltip bottom>
-                    <v-btn icon large :href="source" target="_blank" slot="activator">
                       <v-icon large></v-icon>
-                    </v-btn>
                   </v-tooltip>
                 </v-toolbar>
                 <v-card-text>
                   <v-form>
-                    <v-text-field v-model="login.name" name="login" label="Login" type="text"></v-text-field>
-                    <v-text-field v-model="login.password" name="password" label="Password" id="password" type="password"></v-text-field>
+                    <v-text-field v-model="datas.userName" name="login" label="Login" type="text"></v-text-field>
+                    <v-text-field v-model="datas.password" name="password" label="Password" id="password" type="password"></v-text-field>
                   </v-form>
                 </v-card-text>
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="primary">Login</v-btn>
+                  <v-btn color="primary" @click="login" >Login</v-btn>
                 </v-card-actions>
               </v-card>
             </v-flex>
@@ -36,22 +34,23 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { loginModel } from '../../models/login.model';
 
-const login = new Vue({
-  data: () => ({
-    name: 'asd',
-    password: 'asd',
-  }),
 
-  mounted() {
-    console.log(this.$data.name);
-    //console.log(this.source); // I'm text inside the component.
-  }
-});
 
 @Component
 export default class LoginFields extends Vue {
+    @Prop({
+    default: () => ({
+      userName: "",
+      password: ""
+    })
+  })
+private datas! : loginModel;
 
+login(){
+  console.log(this.datas);
+}
 
 }
 </script>
