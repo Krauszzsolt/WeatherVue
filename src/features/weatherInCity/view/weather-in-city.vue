46e0782e80962f2446e3fbbe8749456e94633290
krausz.zsolt <krausz.zsolt@DEV-NB-0119>
2019-07-18 17:02:04 +0200

    table display

--- src/features/weatherInCity/view/weather-in-city.vue
+++ src/features/weatherInCity/view/weather-in-city.vue
@@ -6,71 +6,77 @@
       <span>YeeYoo</span>
     </v-btn>
 
-    <v-container fluid grid-list-md>
-      <v-data-iterator
-        :items="city"
-        content-tag="v-layout"
-        hide-actions
-        row
-        wrap
-      >
-        <template v-slot:header>
-          <v-toolbar class="mb-2" color="indigo darken-5" dark flat>
-            <v-toolbar-title>This is a header</v-toolbar-title>
-          </v-toolbar>
-        </template>
-        <template >
-          <v-flex xs12 sm6 md4 lg3>
-            <v-card>
-              <v-card-title class="subheading font-weight-bold">asd</v-card-title>
+    <v-container :key="KEY" fluid grid-list-md>
+      <template>
+        <v-toolbar class="mb-2" color="indigo darken-5" dark flat>
+          <v-toolbar-title v-if="!!(city)" >{{city.name}}</v-toolbar-title>
+        </v-toolbar>
+      </template>
+      <template >
+        <div class = "tables">
+        <v-flex xs12 sm6 md4 lg3 v-if="!!(city)">
+          <v-card>
+            <v-card-title class="subheading font-weight-bold">Alapadatok</v-card-title>
 
-              <v-divider></v-divider>
+            <v-divider></v-divider>
 
-              <v-list dense>
-                <v-list-tile>
-                  <v-list-tile-content>Calories:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
+            <v-list dense>
 
-                <v-list-tile>
-                  <v-list-tile-content>Fat:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-content>Hőmérsékelet :</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.main.temp}}</v-list-tile-content>
+              </v-list-tile>
+
+              <v-list-tile>
+                <v-list-tile-content>Hőmérsékelet-minimum :</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.main.temp_min}}</v-list-tile-content>
+              </v-list-tile>
 
                 <v-list-tile>
-                  <v-list-tile-content>Carbs:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
+                <v-list-tile-content>Hőmérsékelet-maximum :</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.main.temp_max}}</v-list-tile-content>
+              </v-list-tile>
+                <v-list-tile>
+                <v-list-tile-content>Nyomás :</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.main.pressure}}</v-list-tile-content>
+              </v-list-tile>
 
                 <v-list-tile>
-                  <v-list-tile-content>Protein:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
+                <v-list-tile-content>Páratartalom :</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.main.humidity}}</v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+            
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm6 md4 lg3 v-if="!!(city)">
+          <v-card>
+            <v-card-title class="subheading font-weight-bold">Koordinatak</v-card-title>
 
-                <v-list-tile>
-                  <v-list-tile-content>Sodium:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
+            <v-divider></v-divider>
 
-                <v-list-tile>
-                  <v-list-tile-content>Calcium:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
+            <v-list dense>
 
-                <v-list-tile>
-                  <v-list-tile-content>Iron:</v-list-tile-content>
-                  <v-list-tile-content class="align-end">asd</v-list-tile-content>
-                </v-list-tile>
-              </v-list>
-            </v-card>
-          </v-flex>
-        </template>
-        <template v-slot:footer>
-          <v-toolbar class="mt-2" color="indigo" dark dense flat>
-            <v-toolbar-title class="subheading">This is a footer</v-toolbar-title>
-          </v-toolbar>
-        </template>
-      </v-data-iterator>
+              <v-list-tile>
+                <v-list-tile-content>Hosszúsági:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.coord.lon}}</v-list-tile-content>
+              </v-list-tile>
+
+              <v-list-tile>
+                <v-list-tile-content>Széllességi:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{city.coord.lat}}</v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+            
+          </v-card>
+        </v-flex>
+        </div>
+      </template>
+      <template footer>
+        <v-toolbar class="mt-2" color="indigo" dark dense flat>
+          <v-toolbar-title class="subheading">This is a footer</v-toolbar-title>
+        </v-toolbar>
+      </template>
     </v-container>
   </div>
 </template> 
@@ -79,7 +85,7 @@
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { Api } from "@/features/services/api";
-import { Clouds, List } from "../../models/weather.model";
+import { Clouds, List, Coord } from '../../models/weather.model';
 
 @Component({
   components: {}
@@ -87,14 +93,25 @@ import { Clouds, List } from "../../models/weather.model";
 export default class Weather extends Vue {
   @Prop({ default: () => localStorage.getItem("CityID") })
   private cityID!: string;
-
+  private KEY: number = 0;
   private city!: List;
+
   get() {
     console.log(this.cityID);
     Api.Cities.getCityById(this.cityID).then(resp => {
       this.city = resp.data;
       console.log(this.city);
+      this.KEY--;
     });
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.tables{
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+</style>
+
